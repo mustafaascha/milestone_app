8b4a0a82965485280748cb9ed1452424f273734d
Mustafa Ascha <mustafa.ascha@gmail.com>
2017-06-14 11:05:12 -0400

    multiple y values

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from bokeh.embed import components
 
 app = Flask(__name__)
 
-def get_data(requested_ticker, col_to_pull):
+def get_data(requested_ticker):
 
     #requested_ticker = "FB"
     search_params = {"ticker": requested_ticker, 
@@ -17,42 +17,27 @@ def get_data(requested_ticker, col_to_pull):
     dtf = req.get(url = "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json", 
             params = search_params)
 
-    col_names = ['ticker', 
-            'date',
-            'open',
-            'high',
-            'low',
-            'close',
-            'volume',
-            'ex-dividend',
-            'split_ratio',
-            'adj_open',
-            'adj_high',
-            'adj_low',
-            'adj_close',
-            'adj_volume']
+    col_names = [d['name'] for d in dtf.json()['datatable']['columns']]
+    col_dates = [row[col_names.index('date')] for row in dtf.json()['datatable']['data']]
 
-    price = []
-    for row in dtf.json()['datatable']['data']:
-        price.append(row[col_names.index(col_to_pull)])
+    col_values = list()
+    for col_to_pull in col_names:
+        col_values.append([row[col_names.index(col_to_pull)] for row in dtf.json()['datatable']['data']])
+    
+    col_dates = pd.to_datetime(col_dates)
 
-    open_date = []
-    for row in dtf.json()['datatable']['data']:
-        open_date.append(row[1])
-
-    open_date = pd.to_datetime(open_date)
-
-    return([open_date, price])
+    return(col_names, col_values, col_dates)
 
 
-
-def make_plot(open_date, price):
+def make_plot(col_names, col_values, col_dates, cols):
     plot = Figure(
             title = 'Data from Quandl WIKI set', 
             x_axis_label = 'date',
             x_axis_type = 'datetime')
 
-    plot.line(price, open_date)
+    for col in cols: 
+        plot.line(col_dates, 
+                col_values[col_names.index(col)])
 
     return(plot)
 
@@ -65,20 +50,33 @@ def main():
 @app.route('/index', methods = ['GET', 'POST'])
 def index():
     #return render_template('index.html')
-    app.ticker = request.form.get('ticker')
-    return render_template('index.html', ticker = app.ticker)
+#    app.ticker = request.form.get('ticker')
+    return render_template('index.html')#, ticker = app.ticker)
   
 
 @app.route('/graph', methods = ['GET', 'POST'])
 def graph():
-    open_date, price = get_data(request.args.get('ticker'), 
-            col_to_pull = request.args.get('xaxis'))
-    plot = make_plot(price, open_date)
+
+    #names of available checkboxes
+    cols_to_check = ['open', 'high', 'low', 'close', 'volume']
+
+    #get values for those boxes
+    cols_to_pull = []
+    for col in cols_to_check: 
+        if request.args.get(col):
+            cols_to_pull.append(col)
+        else: 
+            pass
+    
+    col_names, col_values, col_dates = get_data(request.args.get('ticker'))
+
+    plot = make_plot(col_names, col_values, col_dates, cols_to_pull)
     script, div = components(plot)
+
     return render_template('graph.html', 
             script = script, 
             div = div, 
-            xaxis = request.args.get('xaxis'))
+            xaxis = cols_to_pull)
 
 if __name__ == '__main__':
   app.run(port=33507, debug = True)
